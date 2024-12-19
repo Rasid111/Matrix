b3d5c581ea92e3afe00612115f35138889484366
Rasid <farzrash.rf@gmail.com>
2024-12-19 22:50:48 +0400

    react-bookapp

---
+++ react-bookapp/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero() {
+    return(
+        <>
+            <div style={{backgroundImage: `url('../../public/assets/img/page-title-bg-1.png')`}} className="container-fluid text-white">
+                <div className="row">
+                    <div className="col-2"></div>
+                    <div className="col-3 py-5">
+                        <h1 className='my-3'>Service Details</h1>
+                        <span className='my-3'>Home/Service Details</span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero;
